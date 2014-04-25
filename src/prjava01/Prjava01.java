57117d648836067b4d4c04a23b41dd03e017e5be
Alumne <Alumne@192.168.17.89>
2014-04-25 10:56:35 +0200

    Versión 0.2 del proyecto prjava01

--- src/prjava01/Prjava01.java
+++ src/prjava01/Prjava01.java
@@ -23,7 +23,8 @@ public static void main(String[]args)throws IOException{
 		bw.write("Novap&agrave;ginaweb");bw.newLine(); 
 		bw.write("</title>");bw.newLine(); 
 		bw.write("</head>");bw.newLine(); 
-		bw.write("<body>");bw.newLine(); 
+		bw.write("<body>");bw.newLine();
+                bw.write("<h1>DAW2m08uf4pr2</h1>");bw.newLine();
 		bw.write("Novap&agrave;ginaweb");bw.newLine(); 
 		bw.write("</body>");bw.newLine(); 
 		bw.write("</html>");bw.newLine(); 
